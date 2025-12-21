50278899ea7022fb6dac00cba3dc25a4ebc1b5a4
Prince Kumar Singh <princekumarokok@gmail.com>
2025-12-21 11:18:17 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,18 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
-import VendorDashboard from "./pages/VendorDashboard";
+
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <Routes> 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/vendor-dashboard" element={<VendorDashboard />} />
       </Routes>
   );
 }
